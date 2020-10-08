5a07b06e30e8965b4d6c7ff451e950672cdcb62a
Kaid22 <avdickaid@gmail.com>
2020-10-08 13:45:03 +0200

    commit

--- Primer4/Form1.cs
+++ Primer4/Form1.cs
@@ -16,5 +16,48 @@ namespace Primer4
         {
             InitializeComponent();
         }
+        int a, b, x, y;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            x += 5;
+            Refresh();
+            if (x > ClientRectangle.Width + 2 * a)
+                x = -2 * a;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Point p1 = new Point(x - 2 * a, y);
+            Point p2 = new Point(x + 2 * a, y);
+            Point p3 = new Point(x + a, y + b);
+            Point p4 = new Point(x - a, y + b);
+            Point p5 = new Point(x, y - 3*b);
+            Point p6 = new Point(x + 2 * a, y-2*b);
+            Point p7 = new Point(x, y-b);
+            Point[] camac = { p1, p2, p3, p4 };
+            Point[] jedro = { p5, p6, p7 };
+            Graphics g = CreateGraphics();
+            SolidBrush cetka = new SolidBrush(Color.PeachPuff);
+            g.FillPolygon(cetka, camac);
+            Pen olovka = new Pen(Color.Black, 2);
+            g.DrawPolygon(olovka, camac);
+            cetka.Color = Color.White;
+            g.FillPolygon(cetka, jedro);
+            olovka.Color = Color.Chocolate;
+            olovka.Width = 10;
+            g.DrawLine(olovka, x, y, x, y - 3 * b);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            a = 40;
+            b = 40;
+            x = -2 * a;
+            y = ClientRectangle.Height / 2;
+            timer1.Start();
+
+
+        }
     }
 }
